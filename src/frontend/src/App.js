e1a7fad5662c301193ab5555af93b2b9f2b3d5a4
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-04-06 14:12:37 +0100

    Add slider layout and table form antd in project. Populate form with student data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,54 @@
+import { Layout, Menu, Breadcrumb, Table, Spin, Empty } from 'antd';
+import {
+    DesktopOutlined,
+    PieChartOutlined,
+    FileOutlined,
+    TeamOutlined,
+    UserOutlined, LoadingOutlined,
+} from '@ant-design/icons';
 import { useState, useEffect } from 'react'
 import React from 'react';
 import './App.css';
-import {getAllStudents} from "./client";
+import { getAllStudents } from "./client";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+];
+
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
 
 function App() {
-const [students, setStudents] = useState([])
+    const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+    const [fetching, setFetching] = useState(true);
 
     const fetchStudents = () => {
         getAllStudents()
@@ -12,6 +56,7 @@ const [students, setStudents] = useState([])
             .then(data => {
                 console.log(data);
                 setStudents(data);
+                setFetching(true);
             })
     }
 
@@ -20,13 +65,63 @@ const [students, setStudents] = useState([])
         fetchStudents();
     }, []);
 
-    if (students.length <= 0) {
-        return "no data";
+    const renderStudents = () => {
+        if (fetching) {
+            <Spin indicator={antIcon} />
+        }
+        if (students.length <= 0) {
+            return <Empty />
+        }
+        return <Table dataSource={students}
+                      columns={columns}
+                      bordered
+                      title={() => 'Students'}
+                      pagination={{ pageSize: 50 }}
+                      scroll={{ y: 240 }}
+                      rowKey={(student) => student.id}
+        />;
+
     }
 
-    return students.map((student, index) => {
-       return <p key={index}>{student.id} {student.name} {student.gender} {student.age} {student.email}</p>;
-    })
+    return <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed}
+               onCollapse={setCollapsed}>
+            <div className="logo" />
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <Menu.Item key="1" icon={<PieChartOutlined />}>
+                    Option 1
+                </Menu.Item>
+                <Menu.Item key="2" icon={<DesktopOutlined />}>
+                    Option 2
+                </Menu.Item>
+                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                    <Menu.Item key="3">Tom</Menu.Item>
+                    <Menu.Item key="4">Bill</Menu.Item>
+                    <Menu.Item key="5">Alex</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                </SubMenu>
+                <Menu.Item key="9" icon={<FileOutlined />}>
+                    Files
+                </Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout className="site-layout">
+            <Header className="site-layout-background" style={{ padding: 0 }} />
+            <Content style={{ margin: '0 16px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                    {renderStudents()}
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>By V1NC3 ©2021</Footer>
+        </Layout>
+    </Layout>
 
 }
 
